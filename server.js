02334644304f3e40c3365ded5d4419d041522553
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2017-07-17 07:58:42 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+//****After running your build, uncomment this to launch the react fromtend with your node backend
+// app.use(express.static(__dirname+ '/build'));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  console.log('app!!');
+});
+app.listen(3001, function () {
+  console.log('Example app listening on port 3001!');
+});
