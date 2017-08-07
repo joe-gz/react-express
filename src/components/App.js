5380297e0cf23a01b4746e87e08b13e9105c88b8
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2017-08-06 21:16:39 -0400

    adds react router to front end

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
+import { Switch, Route } from 'react-router-dom';
+import Home from './Home/Home';
+import About from './About/About';
+import NotFound from './Errors/NotFound';
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Switch>
+          <Route exact path='/' component={Home}/>
+          <Route path='/about' component={About}/>
+          <Route path='*' component={NotFound}/>
+        </Switch>
       </div>
     );
   }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <div className='Home'>
+        Hello from the Home page
+      </div>
+    );
+  }
+}
+
+export default Home;
