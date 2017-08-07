7b2c95c5969ec9b63fab78c945afa59421e8ed01
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2017-08-06 21:29:25 -0400

    allows for server side rendering

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
 //****After running your build, uncomment this to launch the react fromtend with your node backend
-// app.use(express.static(__dirname+ '/build'));
+app.use(express.static(__dirname+ '/build'));
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-  console.log('app!!');
+// Always return the main index.html, so react-router render the route in the client
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
 });
+
 app.listen(3001, function () {
   console.log('Example app listening on port 3001!');
 });
