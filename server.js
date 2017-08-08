b4acdd093a68a2d74c44c1cf43cacaae2b0df74a
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2017-08-07 23:25:31 -0400

    adds simple sequelize test

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres:///sequelize_test');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 //****After running your build, uncomment this to launch the react fromtend with your node backend
 // app.use(express.static(__dirname+ '/build'));
