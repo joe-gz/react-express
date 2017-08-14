57c6ceedee5368e8248c2d53af4852c48f9c878e
Joe Glatman Zaretsky <jglatmanzaretsky@gmail.com>
2017-08-13 21:26:36 -0400

    changes app js

--- src/components/App.js
+++ src/components/App.js
@@ -13,13 +13,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div onClick={this.openModal}>
+          Click for redux action
         </div>
-        <p className="App-intro" onClick={this.openModal}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
